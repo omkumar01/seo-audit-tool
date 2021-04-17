c86212059439214580b2c30955bb035af55a3ab6
om <omkumarsahu.26@gmail.com>
2021-04-17 20:05:04 +0530

    added something

--- offpage/abstractapi_preview/main.py
+++ offpage/abstractapi_preview/main.py
@@ -1,6 +1,6 @@
 import requests
 
-params = {"capture_full_page": "False", "width": 393, "height": 851}
+params = {"capture_full_page": "False", "width": 1920, "height": 1080}
 
 response = requests.get(
     "https://screenshot.abstractapi.com/v1/?api_key=ba5121e7767b40cfa23a1c8d5f7d81c7&url=https://heapoftech.live",
@@ -10,5 +10,5 @@ print(response)
 
 img = response.content
 
-f = open("desktop.jpeg", "wb")
+f = open("mobile.jpeg", "wb")
 f.write(img)
